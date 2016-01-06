87bb6adbc7f502c5bc0d44def31cbaf2cb9d0281
Casey Dwyer <caseydwyer@gmail.com>
2016-01-06 23:47:03 +0800

    android.sh: Update.
    Signed-off-by: Casey Dwyer <caseydwyer@gmail.com>

--- profile.d/android.sh
+++ profile.d/android.sh
@@ -1,14 +1,17 @@
-android_paths=
-export ANDROIDPATH=/usr/local/android-sdk
+if [ $(uname) = 'Darwin' ]; then
+    ANDROID_HOME="$HOME/Library/Android/sdk"
+fi
 
-# Add Android SDK to path.
-if [ -e $ANDROIDPATH ]; then
+if [ -d "$ANDROID_HOME" ]; then
+    PATH="$PATH:$ANDROID_HOME/tools"
+    PATH="$PATH:$ANDROID_HOME/platform-tools"
     # Android's sqlite3 doesn't have readline support, so make sure we're using
     # an installed version before adding Android to PATH.
     sqlite3=`which sqlite3`
     if [ -f "$sqlite3" ]; then
         alias sqlite3="$sqlite3"
     fi
-    PATH=$ANDROIDPATH/platform-tools:$PATH
-    PATH=$ANDROIDPATH/tools:$PATH
+else
+    unset ANDROID_HOME
 fi
+
